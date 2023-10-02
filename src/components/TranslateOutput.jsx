ff4d65a324365440c4e471498a24a28510c99fe4
Rhys P. Judd <rhyspjudd.dev@gmail.com>
2023-10-02 06:43:05 +0100

    Updated BG

--- src/components/TranslateOutput.jsx
+++ src/components/TranslateOutput.jsx
@@ -2,31 +2,30 @@ import React, { useState, useEffect } from 'react';
 import Typewriter from "typewriter-effect";
 
 function TranslateOutput({ translatedText }) {
+  
     const [localTranslatedText, setLocalTranslatedText] = useState(translatedText);
-
-    console.log(translatedText)
 
     useEffect(() => {
         setLocalTranslatedText(translatedText);
       }, [translatedText]);
 
-  return (
-   <div>
-      <h3 className='text-lg uppercase font-mono font-bold text-center mt-8'>Translated Text</h3>
-      <div className="chat chat-start">
-        <div className="chat-bubble bg-accent text-white">
-        <Typewriter
-         options={{
-            strings: translatedText,
-            autoStart: true,
-            loop: false,
-            pauseFor: 2500
-        }}
-      />
+    return (
+    <div>
+        <h3 className='text-lg uppercase font-mono font-bold text-center mt-8'>Translated Text</h3>
+        <div className="chat chat-start">
+          <div className="chat-bubble bg-accent text-white">
+          <Typewriter
+              options={{
+                strings: translatedText,
+                autoStart: true,
+                loop: false,
+                pauseFor: 2500
+            }}
+          />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default TranslateOutput;
